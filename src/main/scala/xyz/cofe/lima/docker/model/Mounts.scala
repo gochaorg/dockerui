94e0e8328f190628ae8c3ee541ed35ca38443932
Georgiy Kamnev <nt.gocha@gmail.com>
2022-11-08 03:03:53 +0500

    fix json

--- src/main/scala/xyz/cofe/lima/docker/model/Mounts.scala
+++ src/main/scala/xyz/cofe/lima/docker/model/Mounts.scala
@@ -4,13 +4,13 @@ import tethys.derivation.semiauto.{jsonReader, jsonWriter}
 import tethys.{JsonReader, JsonWriter}
 
 case class Mounts(
-                   Name: String,
-                   Source: String,
-                   Destination: String,
-                   Driver: String,
-                   Mode: String,
-                   RW: Boolean,
-                   Propagation: String,
+                   Name: Option[String],
+                   Source: Option[String],
+                   Destination: Option[String],
+                   Driver: Option[String],
+                   Mode: Option[String],
+                   RW: Option[Boolean],
+                   Propagation: Option[String],
                  )
 object Mounts {
   implicit val reader: JsonReader[Mounts] = jsonReader[Mounts]
