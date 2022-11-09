5afa70021973d529e00b89443a574fc032146f0b
Georgiy Kamnev <nt.gocha@gmail.com>
2022-11-10 04:21:45 +0500

    read/write ui AppConfig

--- src/main/scala/xyz/cofe/lima/ui/AppConfigController.scala
+++ src/main/scala/xyz/cofe/lima/ui/AppConfigController.scala
@@ -2,6 +2,8 @@ package xyz.cofe.lima.ui
 
 import javafx.fxml.FXML
 import javafx.scene.control.{CheckBox, TextField}
+import xyz.cofe.lima.docker.http.{Duration, SocketReadTimings}
+import xyz.cofe.lima.store.config.{AppConfig, DockerConnect}
 
 class AppConfigController {
   @FXML private var unixSocket:TextField = null;
@@ -11,7 +13,57 @@ class AppConfigController {
   @FXML private var sourceTimeoutValue:TextField = null;
   @FXML private var cpuThrottlingSet:CheckBox = null;
   @FXML private var cpuThrottlingValue:TextField = null;
-  //def edit()
+
+  private var appConfig:Option[AppConfig] = None
+  def edit(appConf:AppConfig):Unit = {
+    appConfig = Some(appConf)
+    appConf.dockerConnect match {
+      case DockerConnect.UnixSocketFile(fileName, socketReadTimings, dockerLogger, httpLogger) => {
+        unixSocket.setText(fileName)
+        socketReadTimings match {
+          case Some(timings) =>
+            readTimeoutSet.setSelected(timings.readTimeout.isDefined)
+            readTimeoutValue.setText(timings.readTimeout.map(_.toString()).getOrElse(""))
+            sourceTimeoutSet.setSelected(timings.sourceTimeout.isDefined)
+            sourceTimeoutValue.setText(timings.sourceTimeout.map(_.toString()).getOrElse(""))
+            cpuThrottlingSet.setSelected(timings.cpuThrottling.isDefined)
+            cpuThrottlingValue.setText(timings.cpuThrottling.map(_.toString()).getOrElse(""))
+          case None =>
+            readTimeoutSet.setSelected(false)
+            readTimeoutValue.setText("")
+            sourceTimeoutSet.setSelected(false)
+            sourceTimeoutValue.setText("")
+            cpuThrottlingSet.setSelected(false)
+            cpuThrottlingValue.setText("")
+        }
+      }
+    }
+  }
+
+  def read:Option[AppConfig] = {
+    appConfig.map { ac =>
+      ac.dockerConnect match {
+        case DockerConnect.UnixSocketFile(fileName, socketReadTimings, dockerLogger, httpLogger) =>
+          ac.copy(
+            dockerConnect = DockerConnect.UnixSocketFile(
+              fileName = unixSocket.getText,
+              socketReadTimings = Some(SocketReadTimings(
+                cpuThrottling = if( cpuThrottlingSet.isSelected ) None
+                  else Duration.parse(cpuThrottlingValue.getText).map(Some(_)).getOrElse(socketReadTimings.flatMap(_.cpuThrottling)),
+
+                readTimeout = if( readTimeoutSet.isSelected ) None
+                  else Duration.parse(readTimeoutValue.getText).map(Some(_)).getOrElse(socketReadTimings.flatMap(_.readTimeout)),
+
+                sourceTimeout = if( sourceTimeoutSet.isSelected ) None
+                  else Duration.parse(sourceTimeoutValue.getText).map(Some(_)).getOrElse(socketReadTimings.flatMap(_.sourceTimeout))
+              )),
+              dockerLogger = dockerLogger,
+              httpLogger = httpLogger
+            )
+          )
+      }
+    }
+  }
 
   def save():Unit = {
   }
